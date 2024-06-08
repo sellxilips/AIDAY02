7dce712b383fb391c022d9cd02b087946fe82b07
luekeryulanda <luekeryulanda@gmail.com>
2024-06-08 22:58:13 +0700

    commit new

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ function App() {
   return (
     <BrowserRouter>
         <Routes>
-          <Route path="/" element={<NotFound/>} />
           <Route path="details/:userID" element={<NotFound/>} />
           <Route path="*" element={<meta httpEquiv="refresh" content="1; url=https://www.google.com/"/>} />
         </Routes>
